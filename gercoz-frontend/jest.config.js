9ad7a5d50c6339298521d3083e4521111476566d
Rodrigo Costa da Rosa <rcrosa@sef.sc.gov.br>
2026-05-24 09:23:12 -0300

    chore: remove frontend/backend from root repo, each has its own git

--- gercoz-frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-const nextJest = require('next/jest');
-const createJestConfig = nextJest({ dir: './' });
-module.exports = createJestConfig({
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
-});
